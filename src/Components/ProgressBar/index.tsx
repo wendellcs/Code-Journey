7add3fb8deb41e983f6b1a3d08756ec38d2dbb8a
wendellcs <wendellcs131@gmail.com>
2026-07-22 15:29:25 -0300

    fix: Fix progress bar indicator

--- src/Components/ProgressBar/index.tsx
+++ src/Components/ProgressBar/index.tsx
@@ -8,8 +8,8 @@ export const ProgressBar = ({ progress, wrapperStyles, progressBarStyles }: Prog
     return (
         <div className="w-full text-right" >
             <p className="text-lg">{progress} %</p>
-            <div className={`${wrapperStyles} w-25 h-3`}>
-                <div className={`${progressBarStyles} rounded-md h-3 w-full`}></div>
+            <div className={`${wrapperStyles} w-full rounded-md h-3`}>
+                <div className={`${progressBarStyles} rounded-md h-3`} style={{width: `${progress}%`}}></div>
             </div>
         </div>
     )

4441bb7b2d1f18ff411b1a54c585fa46b58fee77
wendellcs <wendellcs131@gmail.com>
2026-08-11 22:50:47 -0300

    fix: correct aurora svg syntax

--- src/Components/Visuals/Aurora/AuroraFour.tsx
+++ src/Components/Visuals/Aurora/AuroraFour.tsx
@@ -2,15 +2,15 @@ export const AuroraFour = () => {
     return (
         <svg className="w-screen h-auto" width="1440" height="3094" viewBox="0 0 1440 3094" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g filter="url(#filter0_fn_91_348)">
-                <path d="M-85.5 526.47C-85.5 526.47 125.309 346.493 214.5 319.47C345.285 279.843 433.121 304.026 563.5 344.97C675.053 380.001 711.332 466.112 826 488.97C942.605 512.213 1015.65 417.013 1128.5 454.47C1234.69 489.717 1342.5 644.97 1342.5 644.97C1342.5 644.97 1413.08 762.073 1447 842.47C1488.69 941.276 1526 1105.47 1526 1105.47L1447 1381.97C1447 1381.97 1354.32 1493.17 1277 1540.97C1175.54 1603.69 1077.57 1554.35 984.5 1628.97C921.337 1679.61 873.757 1715.52 861 1795.47C848.834 1871.71 919 1984.47 919 1984.47C919 1984.47 976.136 2101.55 954.5 2173.47C931.866 2248.7 811 2314.47 811 2314.47C811 2314.47 741.447 2395.37 684.5 2430.97C590.372 2489.81 467.5 2500.97 467.5 2500.97C467.5 2500.97 243 2389.47 131.5 2500.97C20 2612.47 -56.5 2863.97 -56.5 2863.97" stroke="url(#paint0_linear_91_348)" stroke-opacity="0.5" />
+                <path d="M-85.5 526.47C-85.5 526.47 125.309 346.493 214.5 319.47C345.285 279.843 433.121 304.026 563.5 344.97C675.053 380.001 711.332 466.112 826 488.97C942.605 512.213 1015.65 417.013 1128.5 454.47C1234.69 489.717 1342.5 644.97 1342.5 644.97C1342.5 644.97 1413.08 762.073 1447 842.47C1488.69 941.276 1526 1105.47 1526 1105.47L1447 1381.97C1447 1381.97 1354.32 1493.17 1277 1540.97C1175.54 1603.69 1077.57 1554.35 984.5 1628.97C921.337 1679.61 873.757 1715.52 861 1795.47C848.834 1871.71 919 1984.47 919 1984.47C919 1984.47 976.136 2101.55 954.5 2173.47C931.866 2248.7 811 2314.47 811 2314.47C811 2314.47 741.447 2395.37 684.5 2430.97C590.372 2489.81 467.5 2500.97 467.5 2500.97C467.5 2500.97 243 2389.47 131.5 2500.97C20 2612.47 -56.5 2863.97 -56.5 2863.97" stroke="url(#paint0_linear_91_348)" strokeOpacity="0.5" />
             </g>
             <g filter="url(#filter1_fn_91_348)">
-                <path d="M-86 527.153C-86 527.153 124.809 347.177 214 320.153C344.785 280.526 432.621 304.709 563 345.653C674.553 380.685 710.832 466.796 825.5 489.653C942.105 512.896 1015.15 417.696 1128 455.153C1234.19 490.401 1342 645.653 1342 645.653C1342 645.653 1412.58 762.757 1446.5 843.153C1488.19 941.959 1524.05 998.921 1525.5 1106.15C1527.02 1218.44 1446.5 1382.65 1446.5 1382.65C1446.5 1382.65 1353.82 1493.85 1276.5 1541.65C1175.04 1604.38 1077.07 1555.04 984 1629.65C920.837 1680.29 873.257 1716.21 860.5 1796.15C848.334 1872.39 918.5 1985.15 918.5 1985.15C918.5 1985.15 975.636 2102.24 954 2174.15C931.366 2249.39 810.5 2315.15 810.5 2315.15C810.5 2315.15 740.947 2396.05 684 2431.65C589.872 2490.5 554.506 2485.17 467 2501.65C338.05 2525.94 242.5 2390.15 131 2501.65C19.5 2613.15 -57 2864.65 -57 2864.65" stroke="url(#paint1_linear_91_348)" stroke-opacity="0.2" stroke-width="200" />
+                <path d="M-86 527.153C-86 527.153 124.809 347.177 214 320.153C344.785 280.526 432.621 304.709 563 345.653C674.553 380.685 710.832 466.796 825.5 489.653C942.105 512.896 1015.15 417.696 1128 455.153C1234.19 490.401 1342 645.653 1342 645.653C1342 645.653 1412.58 762.757 1446.5 843.153C1488.19 941.959 1524.05 998.921 1525.5 1106.15C1527.02 1218.44 1446.5 1382.65 1446.5 1382.65C1446.5 1382.65 1353.82 1493.85 1276.5 1541.65C1175.04 1604.38 1077.07 1555.04 984 1629.65C920.837 1680.29 873.257 1716.21 860.5 1796.15C848.334 1872.39 918.5 1985.15 918.5 1985.15C918.5 1985.15 975.636 2102.24 954 2174.15C931.366 2249.39 810.5 2315.15 810.5 2315.15C810.5 2315.15 740.947 2396.05 684 2431.65C589.872 2490.5 554.506 2485.17 467 2501.65C338.05 2525.94 242.5 2390.15 131 2501.65C19.5 2613.15 -57 2864.65 -57 2864.65" stroke="url(#paint1_linear_91_348)" strokeOpacity="0.2" strokeWidth="200" />
             </g>
 
             <defs>
-                <filter id="filter0_fn_91_348" x="-165.822" y="218.786" width="1772.34" height="2725.33" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <filter id="filter0_fn_91_348" x="-165.822" y="218.786" width="1772.34" height="2725.33" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
                     <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
                     <feGaussianBlur stdDeviation="40" result="effect1_foregroundBlur_91_348" /> 
                     <feTurbulence type="fractalNoise" baseFrequency="2 2" stitchTiles="stitch" numOctaves="3" result="noise" seed="3179" />
@@ -19,15 +19,15 @@ export const AuroraFour = () => {
                         <feFuncA type="discrete" tableValues="1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 " />
                     </feComponentTransfer>
                     <feComposite operator="in" in2="effect1_foregroundBlur_91_348" in="coloredNoise1" result="noise1Clipped" />
-                    <feFlood flood-color="rgba(0, 0, 0, 0.25)" result="color1Flood" />
+                    <feFlood floodColor="rgba(0, 0, 0, 0.25)" result="color1Flood" />
                     <feComposite operator="in" in2="noise1Clipped" in="color1Flood" result="color1" />
                     <feMerge result="effect2_noise_91_348">
                         <feMergeNode in="effect1_foregroundBlur_91_348" />
                         <feMergeNode in="color1" />
                     </feMerge>
                 </filter>
-                <filter id="filter1_fn_91_348" x="-352.663" y="0" width="2178.18" height="3093.82" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <filter id="filter1_fn_91_348" x="-352.663" y="0" width="2178.18" height="3093.82" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
                     <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
                     <feGaussianBlur stdDeviation="100" result="effect1_foregroundBlur_91_348" />
                     <feTurbulence type="fractalNoise" baseFrequency="2 2" stitchTiles="stitch" numOctaves="3" result="noise" seed="3396" />
@@ -36,7 +36,7 @@ export const AuroraFour = () => {
                         <feFuncA type="discrete" tableValues="1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 " />
                     </feComponentTransfer>
                     <feComposite operator="in" in2="effect1_foregroundBlur_91_348" in="coloredNoise1" result="noise1Clipped" />
-                    <feFlood flood-color="rgba(0, 0, 0, 0.25)" result="color1Flood" />
+                    <feFlood floodColor="rgba(0, 0, 0, 0.25)" result="color1Flood" />
                     <feComposite operator="in" in2="noise1Clipped" in="color1Flood" result="color1" />
                     <feMerge result="effect2_noise_91_348">
                         <feMergeNode in="effect1_foregroundBlur_91_348" />
@@ -44,16 +44,16 @@ export const AuroraFour = () => {
                     </feMerge>
                 </filter>
                 <linearGradient id="paint0_linear_91_348" x1="1074.98" y1="-414.714" x2="-122.081" y2="2775.6" gradientUnits="userSpaceOnUse">
-                    <stop stop-color="#A855F7" />
-                    <stop offset="0.288462" stop-color="#A385E9" />
-                    <stop offset="0.600962" stop-color="#3B82F6" />
-                    <stop offset="0.942308" stop-color="#22C55E" />
+                    <stop stopColor="#A855F7" />
+                    <stop offset="0.288462" stopColor="#A385E9" />
+                    <stop offset="0.600962" stopColor="#3B82F6" />
+                    <stop offset="0.942308" stopColor="#22C55E" />
                 </linearGradient>
                 <linearGradient id="paint1_linear_91_348" x1="1074.5" y1="-414.031" x2="-122.554" y2="2776.29" gradientUnits="userSpaceOnUse">
-                    <stop stop-color="#A855F7" />
-                    <stop offset="0.288462" stop-color="#A385E9" />
-                    <stop offset="0.600962" stop-color="#3B82F6" stop-opacity="0.5" />
-                    <stop offset="0.942308" stop-color="#22C55E" />
+                    <stop stopColor="#A855F7" />
+                    <stop offset="0.288462" stopColor="#A385E9" />
+                    <stop offset="0.600962" stopColor="#3B82F6" stopOpacity="0.5" />
+                    <stop offset="0.942308" stopColor="#22C55E" />
                 </linearGradient>
             </defs>
         </svg>
--- src/Components/Visuals/Aurora/AuroraTwo.tsx
+++ src/Components/Visuals/Aurora/AuroraTwo.tsx
@@ -2,17 +2,17 @@ export const AuroraTwo = () => {
     return (
         <svg width="623" height="716" viewBox="0 0 623 716" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g filter="url(#filter0_f_36_128)">
-                <path d="M3.5 53.4028C3.5 53.4028 34.5768 139.328 76 176.903C106 204.116 289 233.403 289 233.403M289 233.403C289 233.403 470.5 222.403 567 165.403M289 233.403C289 233.403 396.427 317.555 398 403.903C399.534 488.102 317.55 503.021 341 583.903C357.555 641.004 418 655.403 418 655.403" stroke="url(#paint0_linear_36_128)" stroke-width="20" />
+                <path d="M3.5 53.4028C3.5 53.4028 34.5768 139.328 76 176.903C106 204.116 289 233.403 289 233.403M289 233.403C289 233.403 470.5 222.403 567 165.403M289 233.403C289 233.403 396.427 317.555 398 403.903C399.534 488.102 317.55 503.021 341 583.903C357.555 641.004 418 655.403 418 655.403" stroke="url(#paint0_linear_36_128)" strokeWidth="20" />
             </g>
             <defs>
-                <filter id="filter0_f_36_128" x="-55.9026" y="1.52588e-05" width="677.988" height="715.128" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <filter id="filter0_f_36_128" x="-55.9026" y="1.52588e-05" width="677.988" height="715.128" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
                     <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
                     <feGaussianBlur stdDeviation="25" result="effect1_foregroundBlur_36_128" />
                 </filter>
                 <linearGradient id="paint0_linear_36_128" x1="122.406" y1="53.4028" x2="477.037" y2="70.2808" gradientUnits="userSpaceOnUse">
-                    <stop stop-color="#1A0ECA" />
-                    <stop offset="1" stop-color="#84217A" />
+                    <stop stopColor="#1A0ECA" />
+                    <stop offset="1" stopColor="#84217A" />
                 </linearGradient>
             </defs>
         </svg>

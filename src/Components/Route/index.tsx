d83e83815f76b767e87ec5d9517b9bbfe06630e4
wendellcs <wendellcs131@gmail.com>
2026-08-14 02:38:58 -0300

    fix: access dashboard with invalid token without login

--- src/Components/Route/index.tsx
+++ src/Components/Route/index.tsx
@@ -1,12 +1,30 @@
-import { Navigate } from "react-router-dom"
+import { Navigate, useNavigate } from "react-router-dom"
 import { getAccessToken } from "../../Utilities/authService"
+import axios from "axios"
+import { getAuthHeader } from "../../Utilities/authService"
+import { useEffect } from "react"
 
-export const ProtectedRoute = ({children}: {children: React.ReactNode}) => {
+export const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+    const navigate = useNavigate()
     const token = getAccessToken()
 
-    if (!token){
-        return <Navigate to='/login' replace/>
+    if (!token) {
+        return <Navigate to='/login' replace />
     }
+
+    useEffect(() => {
+        const verifyToken = async () => {
+            try {
+                await axios.get(`${import.meta.env.VITE_API_URL}/general/validate_token`, getAuthHeader())
+            } catch (e) {
+                console.error(e)
+                alert('Token inválido')
+                navigate('/login')
+            }
+        }
+
+        verifyToken()
+    }, [token])
 
     return <>{children}</>
 }
